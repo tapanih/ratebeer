ed57a753ea5ed14bc9bef172cf39cedd325dd3cf
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-10-26 15:20:28 +0300

    End of week 6 refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,21 +15,30 @@ class User < ApplicationRecord
   def favorite_beer
     return nil if ratings.empty?
 
-    ratings.order(score: :desc).limit(1).first.beer
+    ratings.order(score: :desc).limit(1).first&.beer
   end
 
   def favorite_style
-    return nil if ratings.empty?
-
-    style_id = ratings.joins(:beer).group(:style_id).average(:score).max_by { |_, v| v }.first
-    Style.find(style_id)
+    favorite(:style)
   end
 
   def favorite_brewery
+    favorite(:brewery)
+  end
+
+  def favorite(grouped_by)
     return nil if ratings.empty?
 
-    brewery_id = ratings.joins(:beer).group(:brewery_id).average(:score).max_by { |_, v| v }.first
-    Brewery.find(brewery_id)
+    grouped_ratings = ratings.group_by { |r| r.beer.send(grouped_by) }
+    averages = grouped_ratings.map do |group, ratings|
+      { group:, score: average_of(ratings) }
+    end
+
+    averages.max_by { |r| r[:score] }[:group]
+  end
+
+  def average_of(ratings)
+    ratings.sum(&:score).to_f / ratings.count
   end
 
   def self.top_by_ratings_count(count)
