3d56c5a544b8433390c9707ede97cd30d9423842
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-12-15 15:40:20 +0200

    week 7 exercise 3

--- app/javascript/custom/breweries.js
+++ app/javascript/custom/breweries.js
@@ -24,6 +24,18 @@ BREWERIES.show = () => {
         table.appendChild(tr);
     });
 }
+
+BREWERIES.sortByName = () => {
+    BREWERIES.list.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
+}
+
+BREWERIES.sortByYear = () => {
+    BREWERIES.list.sort((a, b) => a.year - b.year);
+}
+
+BREWERIES.sortByBeerCount = () => {
+    BREWERIES.list.sort((a, b) => a.beerCount - b.beerCount);
+}
 const handleResponse = (breweries) => {
     BREWERIES.list = breweries;
     BREWERIES.show();
@@ -32,6 +44,24 @@ const breweries = () => {
     if (document.querySelectorAll("#brewerytable").length < 1) {
         return;
     }
+
+    document.getElementById("name").addEventListener("click", (e) => {
+        e.preventDefault();
+        BREWERIES.sortByName();
+        BREWERIES.show();
+    });
+
+    document.getElementById("year").addEventListener("click", (e) => {
+        e.preventDefault();
+        BREWERIES.sortByYear();
+        BREWERIES.show();
+    });
+
+    document.getElementById("beer-count").addEventListener("click", (e) => {
+        e.preventDefault();
+        BREWERIES.sortByBeerCount();
+        BREWERIES.show();
+    })
 
     fetch("breweries.json")
         .then((response) => response.json())
